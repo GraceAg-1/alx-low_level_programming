bc087958f441ffb9b4758e42a7313033f5d34968
Adeleke Grce Ibidun <adelekegrace2002@gmail.com>
2023-04-23 03:00:35 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include<studio.h>
+#include <stdio.h>
 /**
- * main - entry point 
- *
- * Return: Always 0 (Success)
+ * main - A program that writes using the printf function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return(0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
