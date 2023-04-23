4531fa2dec29fe68e96baf1d9a5fac7f29c917d8
Adeleke Grce Ibidun <adelekegrace2002@gmail.com>
2023-04-23 02:07:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<studio.h>
 /**
- * main - entry point 
- *
- * Return: Always 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return:  0 (Success)
  */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
         return(0);
+}
